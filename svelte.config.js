124f1ce8b09805afac284e4c0d6481c1b3701713
Tommi <tommi@niemi.lol>
2021-11-16 15:23:18 +0000

    add unocss import

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,45 @@
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config.js';
-import preprocess from 'svelte-preprocess';
-import static_adapter from '@sveltejs/adapter-static';
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
+import preprocess from "svelte-preprocess";
+import static_adapter from "@sveltejs/adapter-static";
+import Unocss from 'unocss/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
+  extensions: [".svelte", ...mdsvexConfig.extensions],
 
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		mdsvex(mdsvexConfig)
-	],
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		// build target to build folder
-		adapter: static_adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		paths: {
-			assets: '/seer',
-			base: '/seer'
-		}
-	}
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [mdsvex(mdsvexConfig)],
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+    // build target to build folder
+    adapter: static_adapter({
+      pages: "build",
+      assets: "build",
+      fallback: null,
+    }),
+    paths: {
+      assets: "/seer",
+      base: "/seer",
+    },
+    vite: {
+      plugins: [
+        // readme https://github.com/antfu/unocss
+        Unocss({
+          presets: [
+            presetAttributify({ /* preset options */}),
+            presetUno(),
+            // ...custom presets
+          ],
+          rules: [
+            // custom rules if we want more inline classes outside of windicss
+          ]
+        })
+      ]
+    }
+  },
 };
 
 export default config;
