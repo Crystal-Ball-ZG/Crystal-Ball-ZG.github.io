77908c0bb7cd2a7d526c24d3a863072cfa0722d7
Tommi <tommi@niemi.lol>
2021-11-16 23:48:33 +0000

    remove box txt

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -1,5 +1,4 @@
 <div
-	class="w-200px h-100px border-4px border-dashed border-black hover: bg-gradient-to-r from-green-400 to-blue-500"
+	class="w-200px h-100px border-4px border-dashed border-black hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500"
 >
-	box
 </div>
