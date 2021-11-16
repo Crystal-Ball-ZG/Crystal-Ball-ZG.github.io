f2f6f4bbf770ace546a70b07fe965f5ea57f413e
Tommi <tommi@niemi.lol>
2021-11-16 17:59:42 +0000

    add layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export const load = async ({ page }) => {
+		return {
+			props: {
+				key: page.path
+			}
+		};
+	};
+</script>
+
+<script>
+	import 'uno.css';
+	import PageTransition from '$lib/PageTransition.svelte';
+	import Nav from '$lib/components/Nav.svelte';
+	export let key;
+</script>
+
+<Nav />
+<main>
+	<PageTransition refresh={key}>
+		<slot />
+	</PageTransition>
+</main>
