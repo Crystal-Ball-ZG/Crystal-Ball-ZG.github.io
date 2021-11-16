86e723c6675053faa6517fde07b3f1531d29f66e
Tommi <tommi@niemi.lol>
2021-11-16 23:46:47 +0000

    add .git on repo

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ publish(
 	'build', // path to public directory
 	{
 		branch: 'gh-pages',
-		repo: 'https://github.com/Crystal-Ball-ZG/Crystal-Ball-ZG.github.io', // Update to point to your repository
+		repo: 'https://github.com/Crystal-Ball-ZG/Crystal-Ball-ZG.github.io.git', // Update to point to your repository
 		user: {
 			name: 'Tommi Thaumaturge', // update to use your name
 			email: 'tommi@niemi.lol' // Update to use your email
