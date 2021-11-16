06d10add515f2b5405fb1c9d40a7a7bef03ba202
Tommi <tommi@niemi.lol>
2021-11-16 15:08:55 +0000

    add possibility to write script in md files

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,30 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import preprocess from 'svelte-preprocess';
 import static_adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		mdsvex(mdsvexConfig)
+	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		//adapter: node(),
-		adapter: static_adapter(),
-		appDir: "app",
+		// build target to build folder
+		adapter: static_adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		paths: {
+			assets: '/seer',
+			base: '/seer'
+		}
 	}
 };
 
