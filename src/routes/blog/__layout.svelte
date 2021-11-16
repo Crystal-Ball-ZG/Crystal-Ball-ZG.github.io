65da8c99f241f446712052f65aa4bac4f87d2e4a
Tommi <tommi@niemi.lol>
2021-11-16 23:49:10 +0000

    add layout for blog

---
+++ src/routes/blog/__layout.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+	import 'prismjs';
+	// import 'prismjs/themes/prism.css';
+	export const load = async ({ page }) => {
+		const isIndex = page.path === '/blog/' || page.path === '/blog';
+		return {
+			props: {
+				isIndex,
+				key: page.path
+			}
+		};
+	};
+</script>
+
+<script>
+	import PageTransition from '$lib/PageTransition.svelte';
+	// export let isIndex;
+	export let key;
+	import 'prism-themes/themes/prism-dracula.css';
+</script>
+
+<PageTransition refresh={key}>
+	<slot />
+</PageTransition>
