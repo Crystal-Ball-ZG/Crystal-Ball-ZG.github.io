41f30e1038d678dd0926899335b22da80fa82d07
Tommi <tommi@niemi.lol>
2021-11-17 00:10:20 +0000

    change assets and base to zeitgeist-seer

--- svelte.config.js
+++ svelte.config.js
@@ -7,42 +7,42 @@ import { presetAttributify, presetUno } from 'unocss';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
 
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [mdsvex(mdsvexConfig)],
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		// build target to build folder
-		adapter: static_adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		paths: {
-			assets: '/Crystal-Ball-ZG.github.io',
-			base: '/Crystal-Ball-ZG.github.io'
-		},
-		vite: {
-			plugins: [
-				// readme https://github.com/antfu/unocss
-				Unocss({
-					presets: [
-						presetAttributify({
-							/* preset options */
-						}),
-						presetUno()
-						// ...custom presets
-					],
-					rules: [
-						// custom rules if we want more inline classes outside of windicss
-					]
-				})
-			]
-		}
-	}
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [mdsvex(mdsvexConfig)],
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    // build target to build folder
+    adapter: static_adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      assets: "/zeigeist-seer",
+      base: "/zeitgeist-seer",
+    },
+    vite: {
+      plugins: [
+        // readme https://github.com/antfu/unocss
+        Unocss({
+          presets: [
+            presetAttributify({
+              /* preset options */
+            }),
+            presetUno()
+            // ...custom presets
+          ],
+          rules: [
+            // custom rules if we want more inline classes outside of windicss
+          ]
+        })
+      ]
+    }
+  }
 };
 
 export default config;
